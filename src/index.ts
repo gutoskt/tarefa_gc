5d81337bf718170fb9eb460a3c5e61d29641895f
CarlosAugusto <augustovieira@icomp.ufam.edu.br>
2026-09-16 16:11:39 -0400

    secundario

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,9 @@ export function somar(a: number, b: number): number {
   return a + b;
 }
 
+export function diminuir(a: number, b: number): number {
+  return a - b;
+}
+
 console.log(somar(2, 3));
+console.log(diminuir(5, 2));
